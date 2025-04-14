7c9a357bb30cbe2e537126940baae56fb5ed802d
Vinson <vinsonhe2@gmail.com>
2025-04-14 09:19:26 -0700

    Finish fibsRec function

--- recursion/fibonacci.js
+++ recursion/fibonacci.js
@@ -2,10 +2,20 @@ function fibs(number) {
     let array = [0, 1];
 
     while (array.length < number) {
-        array.push(array[array.length - 1] + array[array.length - 2])
+        array.push(array[array.length - 1] + array[array.length - 2]);
     }
 
     return array;
 }
 
-console.log(fibs(15))
+// set array to [0, 1, 1] to prevent any unexpected buds
+function fibsRec(number, array = [0, 1, 1]) {
+    console.log('recursive!')
+    if (array.length === number) return array;
+
+    array.push(array[array.length - 1] + array[array.length - 2]);
+
+    return fibsRec(number, array);
+}
+
+console.log(fibsRec(8));
