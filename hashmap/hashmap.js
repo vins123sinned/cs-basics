ff807aa383212aec6f00429b78ef05d69244c26c
Vinson 2 <vinsonhe2@gmail.com>
2025-04-19 16:18:50 -0700

    Add set method
    Currently has no support for growing bucket when load factor reached

--- hashmap/hashmap.js
+++ hashmap/hashmap.js
@@ -1,7 +1,7 @@
 class HashMap {
     constructor(capacity, loadFactor) {
-        this.capacity = 0;
-        this.loadFactor = 0.8;
+        this.capacity = capacity;
+        this.loadFactor = loadFactor;
         this.array = [];
     }
 
@@ -10,12 +10,50 @@ class HashMap {
 
         const primeNumber = 31;
         for (let i = 0; i < key.length; i++) {
-            hashCode = primeNumber * hashCode + key.charCodeAt(i);
+            hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
         }
 
         return hashCode;
     }
+
+    set(key, value) {
+        const hashCode = this.hash(key);
+
+        // Check if there is an existing value in bucket
+        if (this.array[hashCode]) {
+            let currentNode = this.array[hashCode];
+
+            while (currentNode !== null) {
+                // Update key's value if applicable
+                if (currentNode.key === key) {
+                    return currentNode.value = value;
+                }
+
+                // stop at tail node and set append
+                if (currentNode.next === null) {
+                    return currentNode.next = {
+                        key,
+                        value,
+                        next: null,
+                    }
+                };
+
+                currentNode = currentNode.next;
+            }
+        } else {
+            this.array[hashCode] = {
+                key,
+                value,
+                next: null,
+            };
+        }
+    }
 }
 
-const people = new HashMap();
+const people = new HashMap(16, 0.8);
+
+people.set('Sita', 'Olda valueismo!');
+people.set('Rama', 'test');
+people.set('Sita', 'New valueismo!');
+
 console.log(people);
