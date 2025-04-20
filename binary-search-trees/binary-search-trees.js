a49791ac9580594499f85d1b4bc6790402d06afc
Vinson 2 <vinsonhe2@gmail.com>
2025-04-20 12:44:34 -0700

    Add buildTree method

--- binary-search-trees/binary-search-trees.js
+++ binary-search-trees/binary-search-trees.js
@@ -11,4 +11,40 @@ class Tree {
         this.array = array;
         this.root = null;
     }
+
+    buildTree(array) {
+        // Filter outs duplicate values
+        const filteredArray = [...new Set(array)];
+        this.root = this.buildTreeRec(filteredArray, 0, filteredArray.length - 1);
+    }
+
+    buildTreeRec(array, start, end) {
+        if (start > end) return null;
+
+        const middle = Math.floor((start + end) / 2);
+        const root = new Node(array[middle]);
+        
+        root.left = this.buildTreeRec(array, start, middle - 1);
+        root.right = this.buildTreeRec(array, middle + 1, end);
+
+        return root;
+    }
+
+    prettyPrint(node, prefix = "", isLeft = true) {
+        if (node === null) {
+          return;
+        }
+        if (node.right !== null) {
+          this.prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+        }
+        console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+        if (node.left !== null) {
+          this.prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+        }
+      };
 }
+
+const tree = new Tree();
+console.log(tree.buildTree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]));
+tree.prettyPrint(tree.root);
+console.log(tree.root);
