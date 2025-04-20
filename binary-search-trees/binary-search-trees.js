6c96037b194d664827c71d2fd6a479af3bf3220e
Vinson 2 <vinsonhe2@gmail.com>
2025-04-20 14:45:39 -0700

    Add insert and delete methods
    Also fixed array filter in buildTree to sort the array beforehand.
    If it didn't then the binary tree insert and delete would fall apart!

--- binary-search-trees/binary-search-trees.js
+++ binary-search-trees/binary-search-trees.js
@@ -13,8 +13,8 @@ class Tree {
     }
 
     buildTree(array) {
-        // Filter outs duplicate values
-        const filteredArray = [...new Set(array)];
+        // Filter out duplicate values
+        const filteredArray = [...new Set(array)].sort((num1, num2) => num1 - num2);
         this.root = this.buildTreeRec(filteredArray, 0, filteredArray.length - 1);
     }
 
@@ -41,10 +41,57 @@ class Tree {
         if (node.left !== null) {
           this.prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
         }
-      };
+    };
+
+    insert(currentNode, value) {
+        if (currentNode === null) return new Node(value);
+
+        if (currentNode.data === value) {
+            console.log('Value already exists!');
+            return currentNode;
+        }
+
+        if (value < currentNode.data) {
+            currentNode.left = this.insert(currentNode.left, value);
+        } else if (value > currentNode.data) {
+            currentNode.right = this.insert(currentNode.right, value);
+        }
+
+        return currentNode;
+    }
+
+    getSuccessor(currentNode) {
+        currentNode = currentNode.right;
+
+        while (currentNode !== null && currentNode.left !== null) {
+            currentNode = currentNode.left;
+        }
+
+        return currentNode;
+    }
+
+    delete(currentNode, value) {
+        if (currentNode === null) return currentNode;
+
+        if (value < currentNode.data) {
+            currentNode.left = this.delete(currentNode.left, value);
+        } else if (value > currentNode.data) {
+            currentNode.right = this.delete(currentNode.right, value);
+        } else {
+            if (currentNode.left === null) return currentNode.right;
+            if (currentNode.right === null) return currentNode.left;
+
+            const successor = this.getSuccessor(currentNode);
+            currentNode.data = successor.data;
+            currentNode.right = this.delete(currentNode.right, successor.data);
+        }
+
+        return currentNode;
+    }
 }
 
 const tree = new Tree();
-console.log(tree.buildTree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]));
+tree.buildTree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
+tree.insert(tree.root, 77);
+tree.delete(tree.root, 67)
 tree.prettyPrint(tree.root);
-console.log(tree.root);
